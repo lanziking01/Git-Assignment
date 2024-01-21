bec3b90e30c304c2e230fd26b511fbc01ad5f216
cys2004 <cys2004_cy@163.com>
2024-01-21 20:25:35 +0800

    Finished all level.

---
+++ Stu/2023210745程远硕/main.c
@@ -0,0 +1,13 @@
+//
+// Created by liu on 2023/12/23.
+// Modified by cysnies on 2024/1/21
+//
+#include <stdio.h>
+
+int main() {
+    int i;
+    printf("input your qq number: ");
+    scanf("%d", &i);
+
+    return 0;
+}
